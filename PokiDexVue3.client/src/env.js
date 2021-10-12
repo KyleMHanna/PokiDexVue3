c4e4383dbb26d36402d2185225d843e6a6a18762
Kyle Hanna <hanna.kyle@live.com>
2021-10-12 13:26:50 -0600

    first commit

---
+++ PokiDexVue3.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'kambrodian.us.auth0.com'
+export const audience = 'https://Codeworks-Projects.com'
+export const clientId = 'D0EifSAMVS1uhGrDLJahpj4YDyhwNeXl'
