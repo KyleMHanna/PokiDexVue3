69d77e7a3c14502805909a9cfbb7eaced6859658
Kyle Hanna <hanna.kyle@live.com>
2021-10-12 15:27:56 -0600

    Pokemon drawing leggoooooo

---
+++ PokiDexVue3.client/src/components/PokemonCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="PokemonCard">
+    <div class="card elevation-2">
+      <div class="card-header ">
+        <h1 class="text-center">
+          {{ pokemon.data.name }}
+        </h1>
+        <h3>
+          <img :src="pokemon.data.sprites.other['official-artwork'].front_default" alt="" class="img-fluid">
+        </h3>
+      </div>
+      <div class="card-body scrollable-y" style="max-height:50vh;">
+        <p>Height: {{ pokemon.data.height }} ft</p>
+        <p>weight: {{ pokemon.data.weight }} lbs</p>
+        <p>Base experience: {{ pokemon.data.base_experience }} </p>
+        <p>{{ pokemon.data.stats.map(s => s.stat.name) }}</p>
+        <p>{{ pokemon.data.stats.map(s => s.base_stat) }}</p>
+      </div>
+      <div class="card-footer text-center">
+        <button
+          @click="catchPokemon(pokemon.data, pokemon.data.Id)"
+
+          class="btn rounded-pill text-danger"
+        >
+          Catch
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { pokemonService } from '../services/PokemonService.js'
+export default {
+  props: {
+    pokemon: { type: Object, required: true }
+  },
+  setup(props) {
+    return {
+      async catchPokemon(pokemon, Id) {
+        pokemonService.catchPokemon(pokemon, Id)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- PokiDexVue3.client/src/services/AxiosService.js
+++ PokiDexVue3.client/src/services/AxiosService.js
@@ -4,7 +4,7 @@ export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
-
+// FIXME - might not work with it not being v2
 export const pokeApi = Axios.create({
   baseURL: 'https://pokeapi.co/api/v2/pokemon',
   timeout: 8000
