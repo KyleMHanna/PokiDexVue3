5f64e418ef8c3a48149933d7f5131818b889e60a
Kyle Hanna <hanna.kyle@live.com>
2021-10-12 13:49:35 -0600

    Pokemon service on Server side done ✔️

---
+++ PokiDexVue3/server/services/PokemonService.js
@@ -0,0 +1,20 @@
+import { dbContext } from '../db/DbContext.js'
+
+class PokemonService {
+  async catchPokemon(pokemonData) {
+    const pokemon = await dbContext.Pokemon.create(pokemonData)
+    return pokemon
+  }
+
+  async getMyPokemon(userId) {
+    const pokemon = await dbContext.Pokemon.find({ ownerId: userId })
+    return pokemon
+  }
+
+  async releasePokemon(Id) {
+    const pokemon = await dbContext.Pokemon.findByIdAndDelete(Id)
+    return pokemon
+  }
+}
+
+export const pokemonService = new PokemonService()
